09887f5964094317a35193762bffaafcf167273d
Shiraz7800 <56200497+Shiraz7800@users.noreply.github.com>
2019-10-12 20:35:04 +0100

    made bot get todays news feed on command

--- bot.js
+++ bot.js
@@ -45,15 +45,36 @@ var initialiseLastUpdated = function() {
 
 var getRssFeed = function(url, channel) {
     // make request to get basketball feed
+    var message = "", i, items;
+    var keys;
+    var date;
+    var today = new Date();
+    today.setDate(today.getDate() - 1);
     request(url, (err, res, body) => {
         if(err) { return console.log('error: ' + err); }
-        console.log('statusCode: ' + res.statusCode)
-        console.log(body);
-        xmlParser.parseString(body, function(err, result) {
-            console.dir(result);
-            console.log('Done');
-        });
-    })
+
+        if(res.statusCode === 200) {
+            xmlParser.parseString(body, function(err, result) {
+                items = result.rss.channel[0].item;
+    
+                for(i=0; i < items.length; i++){
+                    //console.log (items[i].pubDate)
+                    date = new Date(items[i].pubDate);
+                    if (date >= today) {
+                        message += items[i].title;
+                        message += items[i].link  + "\n";
+                        channel.send(message);
+                    }
+    
+    
+                } 
+            });
+
+            //update last update
+            botInfo.lastLookUp = (new Date()).toDateString;
+        }
+        
+    });
 };
 
 client.login(auth.token);
